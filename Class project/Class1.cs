3f2b08ae657df9ffb0efaa4e59292d6380fbe125
22006356CTC <22006356CTC@MA-CTC-INST-S06>
2023-05-08 13:58:37 -0400

    Add project files.

---
+++ Class project/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Class_project
+{
+    internal class Connect4chips{ }
+
+
+
+}
+
+
+namespace Connect4
+{
+    public enum DiscType
+    {
+        Empty,
+        Red,
+        Yellow
+    }
+
+    public class Disc : UserControl
+    {
+        private DiscType type;
+
+        public DiscType Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+                UpdateColor();
+            }
+        }
+
+        public Disc()
+        {
+            Width = 75;
+            Height = 75;
+            Background = Brushes.White;
+            BorderThickness = new System.Windows.Thickness(2);
+            BorderBrush = Brushes.Black;
+        }
+
+        private void UpdateColor()
+        {
+            switch (type)
+            {
+                case DiscType.Red:
+                    Background = Brushes.Red;
+                    break;
+                case DiscType.Yellow:
+                    Background = Brushes.Yellow;
+                    break;
+                default:
+                    Background = Brushes.White;
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Class project/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using Connect4;
+using System;
+
+class Connect4
+{
+    const int ROWS = 6;
+    const int COLUMNS = 7;
+
+    static int[,] board = new int[ROWS, COLUMNS];
+
+    static int currentPlayer = 1;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Connect 4!");
+
+        bool gameOver = false;
+
+        while (!gameOver)
+        {
+            PrintBoard();
+
+            int column = GetNextMove();
+
+            int row = GetNextEmptyRow(column);
+
+            board[row, column] = currentPlayer;
+
+            if (CheckForWin(row, column))
+            {
+                Console.WriteLine("Player {0} wins!", currentPlayer);
+                gameOver = true;
+            }
+            else if (CheckForDraw())
+            {
+                Console.WriteLine("Draw!");
+                gameOver = true;
+            }
+
+            currentPlayer = 3 - currentPlayer;
+        }
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    static void PrintBoard()
+    {
+        Console.Clear();
+        for (int row = ROWS - 1; row >= 0; row--)
+        {
+            for (int col = 0; col < COLUMNS; col++)
+            {
+                Console.Write("|");
+                if (board[row, col] == 0)
+                {
+                    Console.Write(" ");
+                }
+                else if (board[row, col] == 1)
+                {
+                    Console.Write("X");
+                }
+                else
+                {
+                    Console.Write("O");
+                }
+            }
+            Console.Write("|\n");
+        }
+        Console.WriteLine(" 1 2 3 4 5 6 7 ");
+    }
+
+    static int GetNextMove()
+    {
+        while (true)
+        {
+            Console.Write("Player {0}, please enter a column (1-7): ", currentPlayer);
+            string input = Console.ReadLine();
+            int column;
+            if (int.TryParse(input, out column) && column >= 1 && column <= 7 && board[ROWS - 1, column - 1] == 0)
+            {
+                return column - 1;
+            }
+            Console.WriteLine("Invalid move, please try again.");
+        }
+    }
+
+    static int GetNextEmptyRow(int column)
+    {
+        for (int row = 0; row < ROWS; row++)
+        {
+            if (board[row, column] == 0)
+            {
+                return row;
+            }
+        }
+        return -1; // column is full
+    }
+
+    private static bool CheckForWin(int row, int column)
+    {
+        // check horizontal
+        int count = 0;
+        for (int col = 0; col < COLUMNS; col++)
+        {
+            if (board[row, col] == currentPlayer)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+            if (count >= 4)
+            {
+                return true;
+            }
+        }
+
+        // check vertical
+        count = 0;
+        for (int r = 0; r < ROWS; r++)
+        {
+            if (board[r, column] == currentPlayer)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+            if (count >= 4)
+            {
+                return true;
+            }
+        }
+
+        // check diagonal
+        count = 0;
+        for (int i = -3; i <= 3; i++)
+        {
+            int r = row + i;
+            int c = column + i;
+        }
+
+        private bool IsBoardFull()
+        {
+            for (int row = 0; row < ROWS; row++)
+            {
+                for (int column = 0; column < COLUMNS; column++)
+                {
+                    if (board[row, column] == DiscType.Empty)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
